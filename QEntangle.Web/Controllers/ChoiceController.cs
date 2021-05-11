4c28d8e3181b6d4773e0a63388c5fa530c4c517d
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2021-05-11 21:48:15 +0200

    Implemented Navigation

--- QEntangle.Web/Controllers/ChoiceController.cs
+++ QEntangle.Web/Controllers/ChoiceController.cs
@@ -2,33 +2,42 @@
 using QEntangle.Web.Extensions;
 using QEntangle.Web.Interfaces;
 using QEntangle.Web.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace QEntangle.Web.Controllers
 {
   public class ChoiceController : Controller
   {
+    #region Fields
+
     private readonly IChoiceRepository choiceRepository;
+
+    #endregion Fields
+
+    #region Constructors
 
     public ChoiceController(IChoiceRepository choiceRepository)
     {
       this.choiceRepository = choiceRepository;
     }
 
-    public ViewResult List()
-    {
-      var data = this.choiceRepository.GetChoices();
-      var vms = data.Select(o => new ChoicesListViewModel.ChoiceEntryViewModel().GetWithDataModel(o)).ToList();
-      var vm = new ChoicesListViewModel { List =  vms };
-      return View(vm);
-    }
+    #endregion Constructors
+
+    #region Methods
 
     public IActionResult Index()
     {
       return View();
     }
+
+    public ViewResult List()
+    {
+      var data = this.choiceRepository.GetChoices();
+      var vms = data.Select(o => new ChoicesListViewModel.ChoiceEntryViewModel().GetWithDataModel(o)).ToList();
+      var vm = new ChoicesListViewModel { List = vms };
+      return View(vm);
+    }
+
+    #endregion Methods
   }
 }
--- QEntangle.Web/Repositorys/ChoiceRepoMock.cs
+++ QEntangle.Web/Repositorys/ChoiceRepoMock.cs
@@ -14,12 +14,12 @@ namespace QEntangle.Web.Repositorys
         new ChoiceData { Name = "Which Energydrink", Options = new string[] { "Original","Watermellon" }, DefinitiveOption = "Watermellon" },
         new ChoiceData { Name = "Eat Cookie", Options = new string[] {"Yes","No" }, DefinitiveOption = "No" },
         new ChoiceData { Name = "Go Walk", Options = new string[] {"Yes","No" }, DefinitiveOption = "No" },
-        new ChoiceData { Name = "Take oportunity", Options = new string[] {"A","B","C" }, DefinitiveOption = "No" },
+        new ChoiceData { Name = "Take oportunity", Options = new string[] {"A","B","C" }, DefinitiveOption = "B" },
         new ChoiceData { Name = "Go on trip", Options = new string[] {"Yes","No" }, DefinitiveOption = "No" },
         new ChoiceData { Name = "Which Energydrink", Options = new string[] {"Original","Watermellon" }, DefinitiveOption = "Watermellon" },
         new ChoiceData { Name = "Eat Cookie", Options = new string[] {"Yes","No" }, DefinitiveOption = "No" },
         new ChoiceData { Name = "Go Walk", Options = new string[] {"Yes","No" }, DefinitiveOption = "No" },
-        new ChoiceData { Name = "Take oportunity", Options = new string[] {"A","B","C" }, DefinitiveOption = "No" },
+        new ChoiceData { Name = "Take oportunity", Options = new string[] {"A","B","C" }, DefinitiveOption = "C" },
         new ChoiceData { Name = "Go on trip", Options = new string[] {"Yes","No" }, DefinitiveOption = "No" }
       };
     }
